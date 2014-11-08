e6d06cc8ba314e91197b432d901f15d61c532b73
Tobias Skoglund <inbox.tskoglund@gmail.com>
2014-11-08 11:20:07 +0100

    Adding bash xorg WMs and vim dotfiles. Includes install script

---
+++ install.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+PKG_MGR_CMD="sudo pacman -S "
+
+function configure_bspwm() {
+	echo "* Configuring bspwm and sxhkd"
+	mkdir -p $HOME/.config/bspwm
+	cp wm/bspwm/bspwmrc $HOME/.config/bspwm/
+	mkdir -p $HOME/.config/sxhkd/
+	cp wm/bspwm/sxhkdrc $HOME/.config/sxhkd/
+	chmod +x $HOME/.config/sxhkd/sxhkdrc
+}
+
+function configure_xorg() {
+	echo "* Configuring xinitrc and Xdefaults"
+	cp xorg/xinitrc $HOME/.xinitrc
+	cp xorg/Xdefaults $HOME/.Xdefaults
+}
+function configure_vim() {
+	echo "* Configuring vimrc"
+	cp vim/vimrc $HOME/.vimrc
+	echo "* Configuring vim color molokai"
+	mkdir -p $HOME/.vim/colors/
+	cp vim/molokai/colors/molokai.vim $HOME/.vim/colors/ 
+}
+
+function configure_bash() {
+	echo "* Configuring bashrc"
+	cp bash/bashrc $HOME/.bashrc
+}
+
+function install_folders() {
+	echo "* Creating folders archive/ src/ projects/ sandbox/" 
+	mkdir -p $HOME/archive/
+	mkdir -p $HOME/src/
+	mkdir -p $HOME/projects/
+	mkdir -p $HOME/sandbox/
+}
+
+
+for thing in $@; do
+	case "$thing" in
+		"vim") 
+		configure_vim
+		;;
+		"bash") 
+		configure_bash
+		;;
+		"xorg") 
+		configure_xorg
+		;;
+		"bspwm")
+		configure_bspwm
+		;;
+		"folders")
+		install_folders
+		;;
+		*) 
+		configure_xorg
+		configure_bspwm
+		configure_vim
+		install_folders
+		;;
+	esac
+done
