39fd8f1a4307af359d2dcf7ac2c5d80d4c53f91d
tobias <tobias@indrome.com>
2015-06-28 13:43:52 +0200

    Fixed colorized output

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+
+function printGreen {
+	printf "${GREEN}$1${NC}"
+}
+function printRed {
+	printf "${RED}$1${NC}"
+}
+
+
 function _fileExistAndEqualTo
 {
 	if [ -f $1 ]; then
@@ -14,39 +27,43 @@ function _fileExistAndEqualTo
 function isBashConfigured
 {
 	if [[ $(_fileExistAndEqualTo "~/.bashrc" "./bash/bashrc") -eq 0 ]]; then
-		echo "[configured]"
+		printGreen "[configured]"
 	else
-		echo "[not configured]"
+		printRed "[not configured]"
 	fi
 }
 function isDunstConfigured
 {
 	if [[ $(_fileExistAndEqualTo "~/.config/dunst/dunstrc" "./dunst/dunstrc") -eq 0 ]]; then
-		echo "[configured]"
+		printGreen "[configured]"
 	else
-		echo "[not configured]"
+		printRed "[not configured]"
 	fi
 }
 function isHLWMConfigured
 {
 	if [[ $(_fileExistAndEqualTo ~/.config/herbstluftwm/autostart ./wm/herbstluftwm/autostart) -eq 0 ]] && \
 	   [[ $(_fileExistAndEqualTo ~/.config/herbstluftwm/panel.sh ./wm/herbstluftwm/panel.sh) -eq 0 ]]; then
-		echo "[configured]"
+		printGreen "[configured]"
 	else
-		echo "[not configured]"
+		printRed "[not configured]"
 	fi
 }
 function isVimConfigured
 {
-	echo $(_fileExistAndEqualTo "~/.vimrc" "./vim/vimrc")
+	if [[ $(_fileExistAndEqualTo "~/.vimrc" "./vim/vimrc") -eq 0 ]]; then
+		printGreen "[configured]"
+	else
+		printRed "[not configured]"
+	fi
 }
 function isXorgConfigured
 {
 	if  [[ $(_fileExistAndEqualTo ~/.xinitrc ./xorg/xinitrc) -eq 0 ]] && \
 		[[ $(_fileExistAndEqualTo ~/.Xresources ./xorg/Xresources) -eq 0 ]]; then
-		echo "[configured]"
+		printGreen "[configured]"
 	else
-		echo "[not configured]"
+		printRed "[not configured]"
 	fi
 }
 
@@ -54,9 +71,9 @@ function _checkIfInstalled
 {
 	pacman -Qi $1 &> /dev/null;
 	if [  $? -eq 0 ]; then
-		echo "[installed]"
+		printGreen "[installed]"
 	else
-		echo "[uninstalled]"
+		printRed "[not installed]"
 	fi
 }
 
@@ -65,6 +82,7 @@ function showInfo
 	echo -e "bash\t\t"$(_checkIfInstalled bash)"\t"$(isBashConfigured)
 	echo -e "dunst\t\t"$(_checkIfInstalled dunst)"\t"$(isDunstConfigured)
 	echo -e "herbstluftwm\t"$(_checkIfInstalled herbstluftwm)"\t"$(isHLWMConfigured)
+	echo -e "Vim\t\t"$(_checkIfInstalled vim)"\t"$(isVimConfigured)
 	echo -e "Xorg\t\t"$(_checkIfInstalled xorg-server)"\t"$(isXorgConfigured)
 
 }
