fa2fe416eb4637c068902b309c7412748489433e
Tobias Ulvgard <tobias.ulvgard@gmail.com>
2015-06-28 16:30:08 +0200

    Adding install stages for certain packages

--- install.sh
+++ install.sh
@@ -2,24 +2,49 @@
 PKG_MGR_CMD="sudo pacman -S "
 
 function configure_bash() {
+	pacman -Qi bash &> /dev/null
+	if [[ ! $? -eq 0 ]]; then
+		echo "* Installing bash"
+		sudo pacman -Syy bash 
+	fi
 	echo "* Configuring bashrc"
 	cp bash/bashrc $HOME/.bashrc
 }
 function configure_dunst {
+	pacman -Qi dunst &> /dev/null
+	if [[ ! $? -eq 0 ]]; then
+		echo "* Installing dunst"
+		sudo pacman -Syy dunst
+	fi
 	echo "* Configuring dunst"
 	mkdir -p $HOME/.config/dunst/
 	cp dunst/dunstrc $HOME/.config/dunst/dunstrc
 }
 function configure_git() {
+	pacman -Qi git &> /dev/null
+	if [[ ! $? -eq 0 ]]; then
+		echo "* Installing git"
+		sudo pacman -Syy git 
+	fi
 	echo "* Configuring git"
 	cp git/gitconfig $HOME/.gitconfig 
 }
 function configure_herbstluftwm() {
+	pacman -Qi herbstluftwm &> /dev/null
+	if [[ ! $? -eq 0 ]]; then
+		echo "* Installing herbstluftwm"
+		sudo pacman -Syy herbstluftwm 
+	fi
 	echo "* Configuring Herbstluftwm"
 	mkdir -p $HOME/.config/herbstluftwm/
 	cp wm/herbstluftwm/* $HOME/.config/herbstluftwm/
 }
 function configure_vim() {
+	pacman -Qi vim-runtime &> /dev/null
+	if [[ ! $? -eq 0 ]]; then
+		echo "* Installing vim"
+		sudo pacman -Syy vim-runtime 
+	fi
 	echo "* Configuring vimrc"
 	cp vim/vimrc $HOME/.vimrc
 	echo "* Configuring vim color molokai"
@@ -30,13 +55,9 @@ function configure_xorg() {
 	echo "* Configuring xinitrc and Xresources"
 	cp xorg/xinitrc $HOME/.xinitrc
 	cp xorg/Xresources $HOME/.Xresources
-
 	echo "* Configuring Xdefaults (urxvt config-file)"
 	cp xorg/Xdefaults-urxvt $HOME/.Xdefaults
 }
-
-
-
 
 function install_folders() {
 	echo "* Creating folders archive/ src/ projects/ sandbox/" 
@@ -45,7 +66,6 @@ function install_folders() {
 	mkdir -p $HOME/projects/
 	mkdir -p $HOME/sandbox/
 }
-
 
 for thing in $@; do
 	case "$thing" in
@@ -72,6 +92,9 @@ for thing in $@; do
 		;;
 		"all") 
 		configure_xorg
+		configure_herbstluftwm
+		configure_git
+		configure_dunst
 		configure_vim
 		install_folders
 		;;
