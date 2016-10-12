2e5769ab7213f8c2a5694bfcf0a20e7c960c7563
Tobias Ulvgard <tobias.ulvgard@gmail.com>
2016-10-12 08:32:03 +0200

    Change monitor setup/teardown script names

--- monitorsetups/setup.sh
+++ monitorsetups/setup.sh
@@ -3,6 +3,6 @@ xrandr --output eDP1 --off
 sleep 3
 xrandr --output DP1-1 --auto  --rotate right --primary
 xrandr --output DP1-2-8 --auto  --right-of DP1-1 --rotate right
-xrandr --output DP1-2-1 --auto  --right-of DP1-2-8
+xrandr --output DP1-2-1 --auto  --right-of DP1-2-8 --rotate left
 
 herbstclient detect_monitors
