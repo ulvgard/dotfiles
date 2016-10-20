46d712425a944b30a3dec2b1fd248d28b0935b61
Tobias Ulvgard <tobias.ulvgard@gmail.com>
2016-10-20 10:00:06 +0200

    Update monitor scrips for DP,DP,VGA settings

--- monitorsetups/setup.sh
+++ monitorsetups/setup.sh
@@ -2,7 +2,7 @@
 xrandr --output eDP1 --off
 sleep 3
 xrandr --output DP1-1 --auto  --rotate right --primary
-xrandr --output DP1-2-8 --auto  --right-of DP1-1 --rotate right
-xrandr --output DP1-2-1 --auto  --right-of DP1-2-8 --rotate left
+xrandr --output DP1-2 --auto  --right-of DP1-1 --rotate right
+xrandr --output DP1-3 --auto  --right-of DP1-2 --rotate left
 
 herbstclient detect_monitors
--- monitorsetups/teardown.sh
+++ monitorsetups/teardown.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 xrandr --output DP1-1 --off
-xrandr --output DP1-2-1 --off
-xrandr --output DP1-2-8 --off
+xrandr --output DP1-2 --off
+xrandr --output DP1-3 --off
 sleep 3
 xrandr --output eDP1 --auto --primary
 herbstclient detect_monitors
